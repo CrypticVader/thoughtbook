4c2391a32db84fccefc90fbb50080ee427543500
CrypticVader <akhilbnair101@gmail.com>
2023-02-20 00:06:27 +0530

    WIP note selection functionality
    - Moved note list item to separate stateless widget.

--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:thoughtbook/extensions/buildContext/loc.dart';
+import 'package:thoughtbook/extensions/buildContext/theme.dart';
 import 'package:thoughtbook/services/cloud/cloud_note.dart';
 import 'package:thoughtbook/utilities/dialogs/delete_dialog.dart';
 import 'package:thoughtbook/utilities/modals/show_note_item_modal_bottom_sheet.dart';
@@ -11,12 +12,16 @@ class NotesListView extends StatefulWidget {
   final Iterable<CloudNote> notes;
   final NoteCallback onDeleteNote;
   final NoteCallback onTap;
+  final NoteCallback onLongPress;
+  final List<CloudNote> selectedNotes;
 
   const NotesListView({
     Key? key,
     required this.notes,
+    required this.selectedNotes,
     required this.onDeleteNote,
     required this.onTap,
+    required this.onLongPress,
   }) : super(key: key);
 
   @override
@@ -26,7 +31,6 @@ class NotesListView extends StatefulWidget {
 class _NotesListViewState extends State<NotesListView> {
   @override
   Widget build(BuildContext context) {
-    var theme = Theme.of(context);
     if (widget.notes.isEmpty) {
       return Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -34,7 +38,7 @@ class _NotesListViewState extends State<NotesListView> {
           Icon(
             Icons.edit_note_rounded,
             size: 100,
-            color: theme.colorScheme.onBackground,
+            color: context.theme.colorScheme.onBackground,
           ),
           Center(
             child: Text(
@@ -52,70 +56,12 @@ class _NotesListViewState extends State<NotesListView> {
         itemCount: widget.notes.length,
         itemBuilder: (context, index) {
           final note = widget.notes.elementAt(index);
-          return Slidable(
-            key: ValueKey(index),
-            endActionPane: ActionPane(
-              motion: const ScrollMotion(),
-              children: [
-                const SizedBox(
-                  width: 8,
-                ),
-                SlidableAction(
-                  flex: 1,
-                  onPressed: (context) async {
-                    final shouldDelete = await showDeleteDialog(context);
-                    if (shouldDelete) {
-                      widget.onDeleteNote(note);
-                    }
-                  },
-                  borderRadius: BorderRadius.circular(64),
-                  backgroundColor: theme.colorScheme.error,
-                  foregroundColor: theme.colorScheme.onError,
-                  icon: Icons.delete_rounded,
-                  label: context.loc.delete,
-                ),
-              ],
-            ),
-            child: Card(
-              elevation: 0,
-              surfaceTintColor: Colors.transparent,
-              color: Colors.transparent,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(24),
-              ),
-              child: ListTile(
-                onTap: () {
-                  widget.onTap(note);
-                },
-                tileColor:
-                    theme.colorScheme.secondaryContainer.withOpacity(0.55),
-                contentPadding: const EdgeInsets.fromLTRB(16, 4, 16, 4),
-                shape: RoundedRectangleBorder(
-                  side: BorderSide(
-                    width: 1.4,
-                    color: theme.colorScheme.onBackground.withOpacity(0.3),
-                  ),
-                  borderRadius: BorderRadius.circular(24),
-                ),
-                title: Text(
-                  note.text,
-                  maxLines: 1,
-                  softWrap: true,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                trailing: IconButton(
-                  tooltip: context.loc.more_options,
-                  onPressed: () async {
-                    showNoteItemModalBottomSheet(
-                      context: context,
-                      note: note,
-                      onDeleteNote: widget.onDeleteNote,
-                    );
-                  },
-                  icon: const Icon(Icons.more_horiz_rounded),
-                ),
-              ),
-            ),
+          return NoteItem(
+            note: note,
+            isSelected: widget.selectedNotes.contains(note),
+            onDeleteNote: widget.onDeleteNote,
+            onTap: widget.onTap,
+            onLongPress: widget.onLongPress,
           );
         },
         separatorBuilder: (BuildContext context, int index) {
@@ -125,5 +71,105 @@ class _NotesListViewState extends State<NotesListView> {
         },
       );
     }
+  }
+}
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({
+    Key? key,
+    required this.note,
+    required this.isSelected,
+    required this.onDeleteNote,
+    required this.onTap,
+    required this.onLongPress,
+  }) : super(key: key);
+
+  final CloudNote note;
+  final bool isSelected;
+  final NoteCallback onDeleteNote;
+  final NoteCallback onTap;
+  final NoteCallback onLongPress;
+
+  Color _getTileColor(BuildContext context) {
+    if (isSelected) {
+      return context.theme.colorScheme.secondaryContainer;
+    } else {
+      return context.theme.colorScheme.secondaryContainer.withOpacity(0.5);
+    }
+  }
+
+  Color _getTileBorderColor(BuildContext context) {
+    if (isSelected) {
+      return context.theme.colorScheme.secondary;
+    } else {
+      return context.theme.colorScheme.onBackground.withOpacity(0.3);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Slidable(
+      key: ValueKey(note),
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          const SizedBox(
+            width: 8,
+          ),
+          SlidableAction(
+            flex: 1,
+            onPressed: (context) async {
+              final shouldDelete = await showDeleteDialog(context);
+              if (shouldDelete) {
+                onDeleteNote(note);
+              }
+            },
+            borderRadius: BorderRadius.circular(64),
+            backgroundColor: context.theme.colorScheme.error,
+            foregroundColor: context.theme.colorScheme.onError,
+            icon: Icons.delete_rounded,
+            label: context.loc.delete,
+          ),
+        ],
+      ),
+      child: Card(
+        elevation: 0,
+        surfaceTintColor: Colors.transparent,
+        color: Colors.transparent,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: ListTile(
+          onTap: () => onTap(note),
+          onLongPress: () => onLongPress(note),
+          tileColor: _getTileColor(context),
+          contentPadding: const EdgeInsets.fromLTRB(16, 4, 16, 4),
+          shape: RoundedRectangleBorder(
+            side: BorderSide(
+              width: 1.4,
+              color: _getTileBorderColor(context),
+            ),
+            borderRadius: BorderRadius.circular(24),
+          ),
+          title: Text(
+            note.text,
+            maxLines: 1,
+            softWrap: true,
+            overflow: TextOverflow.ellipsis,
+          ),
+          trailing: IconButton(
+            tooltip: context.loc.more_options,
+            onPressed: () async {
+              showNoteItemModalBottomSheet(
+                context: context,
+                note: note,
+                onDeleteNote: onDeleteNote,
+              );
+            },
+            icon: const Icon(Icons.more_horiz_rounded),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart' show ReadContext;
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:thoughtbook/constants/routes.dart';
 import 'package:thoughtbook/enums/menu_action.dart';
 import 'package:thoughtbook/extensions/buildContext/loc.dart';
@@ -23,6 +24,7 @@ class NotesView extends StatefulWidget {
 
 class _NotesViewState extends State<NotesView> {
   late final FirebaseCloudStorage _notesService;
+  List<CloudNote> selectedNotes = [];
 
   String get userId => AuthService.firebase().currentUser!.id;
 
@@ -41,44 +43,46 @@ class _NotesViewState extends State<NotesView> {
           style: CustomTextStyle(context).appBarTitle,
         ),
         actions: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed(createOrUpdateNoteRoute);
-            },
-            style: ElevatedButton.styleFrom(
-              shadowColor: Colors.transparent,
-              elevation: 10,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16),
+          // TODO: Use valueListenableBuilder to update the actions depending on the number of items in SelectedNotes
+          if (selectedNotes.isEmpty)
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(createOrUpdateNoteRoute);
+              },
+              style: ElevatedButton.styleFrom(
+                shadowColor: Colors.transparent,
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
               ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 8,
-                horizontal: 0,
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Icon(
-                    Icons.add_rounded,
-                    size: 28,
-                  ),
-                  const SizedBox(
-                    width: 6,
-                  ),
-                  Text(
-                    context.loc.note,
-                    style: const TextStyle(
-                      fontSize: 20,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 0,
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.add_rounded,
+                      size: 28,
                     ),
-                  ),
-                ],
+                    const SizedBox(
+                      width: 6,
+                    ),
+                    Text(
+                      context.loc.note,
+                      style: const TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
           PopupMenuButton<MenuAction>(
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
@@ -90,28 +94,50 @@ class _NotesViewState extends State<NotesView> {
                     context.read<AuthBloc>().add(const AuthEventLogOut());
                   }
                   break;
+                case MenuAction.share:
+                  Share.share(selectedNotes.first.text);
               }
             },
             itemBuilder: (context) {
               return [
-                PopupMenuItem<MenuAction>(
-                  value: MenuAction.logout,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      const Icon(Icons.logout_rounded),
-                      const SizedBox(
-                        width: 6,
-                      ),
-                      Text(
-                        context.loc.logout_button,
-                        style: const TextStyle(
-                          fontSize: 15,
+                if (selectedNotes.isEmpty)
+                  PopupMenuItem<MenuAction>(
+                    value: MenuAction.logout,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const Icon(Icons.logout_rounded),
+                        const SizedBox(
+                          width: 6,
                         ),
-                      ),
-                    ],
+                        Text(
+                          context.loc.logout_button,
+                          style: const TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                )
+                if (selectedNotes.length == 1)
+                  PopupMenuItem<MenuAction>(
+                    value: MenuAction.share,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const Icon(Icons.share_rounded),
+                        const SizedBox(
+                          width: 6,
+                        ),
+                        Text(
+                          context.loc.share_note,
+                          style: const TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
               ];
             },
           ),
@@ -127,14 +153,41 @@ class _NotesViewState extends State<NotesView> {
                 final allNotes = snapshot.data as Iterable<CloudNote>;
                 return NotesListView(
                   notes: allNotes,
+                  selectedNotes: selectedNotes,
                   onDeleteNote: (note) async {
-                    _notesService.deleteNote(documentId: note.documentId);
-                  },
-                  onTap: (CloudNote note) {
-                    Navigator.of(context).pushNamed(
-                      createOrUpdateNoteRoute,
-                      arguments: note,
+                    _notesService.deleteNote(
+                      documentId: note.documentId,
                     );
+                  },
+                  onTap: (note) {
+                    if (selectedNotes.contains(note)) {
+                      setState(
+                        () {
+                          selectedNotes.remove(note);
+                        },
+                      );
+                      return;
+                    } else {
+                      Navigator.of(context).pushNamed(
+                        createOrUpdateNoteRoute,
+                        arguments: note,
+                      );
+                    }
+                  },
+                  onLongPress: (note) {
+                    if (selectedNotes.contains(note)) {
+                      setState(
+                        () {
+                          selectedNotes.remove(note);
+                        },
+                      );
+                    } else {
+                      setState(
+                        () {
+                          selectedNotes.add(note);
+                        },
+                      );
+                    }
                   },
                 );
               } else {
